b297d5c4c102f3fbc95998858f6a66e803db5c94
Arjun Mehta <arjujmehta23@gmail.com>
2022-04-23 00:54:38 -0400

    Improve UI of email and password TextInputs

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -13,6 +13,10 @@ import {
 import React, { useState } from "react";
 import { AuthContext } from "../contexts/UserContext";
 import theme from "../theme";
+import EmailIcon from "../assets/email.svg";
+import PasswordIcon from "../assets/password.svg";
+import VisibilityOnIcon from "../assets/visibilityOn.svg";
+import VisibilityOffIcon from "../assets/visibilityOff.svg";
 
 const { colors, button, margins, fontSizes, paddings, roundedComponent } =
   theme;
@@ -20,7 +24,10 @@ const { colors, button, margins, fontSizes, paddings, roundedComponent } =
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
   const { signIn } = React.useContext(AuthContext);
+  const emailInputRef = React.createRef<TextInput>();
+  const passwordInputRef = React.createRef<TextInput>();
 
   const handleSignIn = (email: string, password: string) => {
     if (!email) {
@@ -32,19 +39,57 @@ const Login = () => {
     }
   };
 
+  const changePasswordVisibility = () => {
+    isPasswordVisible
+      ? setIsPasswordVisible(false)
+      : setIsPasswordVisible(true);
+  };
+
   const renderFooter = () => {
     return (
       <>
         <TouchableOpacity
           onPress={() => handleSignIn(email, password)}
-          style={[styles.roundedComponent, styles.primaryButton]}
+          style={[styles.roundedComponent, styles.button]}
         >
           <Text style={styles.buttonText}>Login</Text>
         </TouchableOpacity>
         <View style={styles.footerContainer}>
-          <Text style={styles.footerText}>New to Coinfacts? </Text>
+          <Text style={styles.footerText}>New to CoinFacts?</Text>
           <Text style={[styles.footerText, styles.registerText]}>Register</Text>
         </View>
+      </>
+    );
+  };
+
+  const renderPasswordVisibilityButton = () => {
+    return (
+      <>
+        {password ? (
+          <>
+            {isPasswordVisible ? (
+              <VisibilityOffIcon
+                width={20}
+                height={20}
+                fill={colors.lightGrey}
+                style={{ alignSelf: "center", marginRight: margins.small }}
+                onPress={() => {
+                  changePasswordVisibility();
+                }}
+              />
+            ) : (
+              <VisibilityOnIcon
+                width={20}
+                height={20}
+                fill={colors.lightGrey}
+                style={{ alignSelf: "center", marginRight: margins.small }}
+                onPress={() => {
+                  changePasswordVisibility();
+                }}
+              />
+            )}
+          </>
+        ) : null}
       </>
     );
   };
@@ -57,27 +102,50 @@ const Login = () => {
           source={require("../assets/welcome.png")}
         />
         <Text style={styles.title}>Login</Text>
-        <TextInput
-          style={styles.roundedComponent}
-          placeholder="Email"
-          autoCapitalize="none"
-          autoCorrect={false}
-          value={email}
-          selectionColor={colors.black}
-          onChangeText={(text) => {
-            setEmail(text);
-          }}
-        />
-        <TextInput
-          style={styles.roundedComponent}
-          placeholder="Password"
-          value={password}
-          selectionColor={colors.black}
-          onChangeText={(text) => {
-            setPassword(text);
-          }}
-          secureTextEntry
-        />
+        <TouchableOpacity
+          style={[styles.roundedComponent, { flexDirection: "row" }]}
+          onPress={() => emailInputRef.current?.focus()}
+        >
+          <EmailIcon
+            width={20}
+            height={20}
+            fill={colors.lightGrey}
+            style={{ alignSelf: "center", marginRight: margins.medium }}
+          />
+          <TextInput
+            placeholder="Email"
+            value={email}
+            ref={emailInputRef}
+            onChangeText={(text) => setEmail(text)}
+            autoCapitalize="none"
+            autoCorrect={false}
+            selectionColor={colors.black}
+            style={styles.inputText}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.roundedComponent, { flexDirection: "row" }]}
+          onPress={() => passwordInputRef.current?.focus()}
+        >
+          <PasswordIcon
+            width={20}
+            height={20}
+            fill={colors.lightGrey}
+            style={{ alignSelf: "center", marginRight: margins.medium }}
+          />
+          <TextInput
+            placeholder="Password"
+            value={password}
+            ref={passwordInputRef}
+            onChangeText={(text) => setPassword(text)}
+            autoCapitalize="none"
+            autoCorrect={false}
+            selectionColor={colors.black}
+            style={styles.inputText}
+            secureTextEntry={isPasswordVisible ? false : true}
+          />
+          {renderPasswordVisibilityButton()}
+        </TouchableOpacity>
       </ScrollView>
       {renderFooter()}
     </KeyboardAvoidingView>
@@ -98,24 +166,26 @@ const styles = StyleSheet.create({
   },
   roundedComponent: {
     height: button.height,
-    paddingHorizontal: paddings.large,
+    padding: paddings.medium,
     marginVertical: margins.medium,
     marginHorizontal: margins.large,
     borderRadius: roundedComponent.borderRadius,
     backgroundColor: colors.translucentGrey,
   },
-  primaryButton: {
+  button: {
     justifyContent: "center",
     backgroundColor: colors.blue,
   },
   buttonText: {
     textAlign: "center",
     fontWeight: "bold",
+    fontSize: fontSizes.medium,
     color: colors.white,
   },
   footerText: {
     marginTop: margins.medium,
     marginBottom: margins.large,
+    fontSize: 14,
     fontWeight: "bold",
     color: colors.lightGrey,
   },
@@ -126,6 +196,10 @@ const styles = StyleSheet.create({
   registerText: {
     color: colors.blue,
     marginHorizontal: margins.small,
+  },
+  inputText: {
+    flex: 1,
+    fontSize: fontSizes.medium,
   },
   imageStyle: {
     height: Dimensions.get("window").width / 1.2,
